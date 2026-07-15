70e346c5c8bc1f27900ee4e2d9be6c647a9cab45
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-07-15 14:12:16 +0200

    Add game start prompt with input validation

--- hardness_racing_simulation.py
+++ hardness_racing_simulation.py
@@ -14,4 +14,15 @@
 # ==============================================================
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
+
+    print("Bienvenue dans l'hippodrome de Saint-Albe. ")
+    choice = input("Voulez-vous jouer à nos course hippique? (O ou N) : ")
+
+    while choice != "O" and choice != "N":
+        choice = input("Vous vous êtes trompés, veuillez réessayer : ")
+
+        while choice == "O":
+            choice = input("Voulez-vous rejouer? (O ou N) : ")
+
+    print("Nous vous remercions d'avoir jouer dans notre hippodrome. A bientôt ! ")
