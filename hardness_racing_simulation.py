1c37520785f71dcae531a911f987b90e3382a2f5
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-07-15 14:37:14 +0200

    create choose_number_of_horses_and_shuffle_them method

--- hardness_racing_simulation.py
+++ hardness_racing_simulation.py
@@ -1,12 +1,27 @@
+import random
 
 # =======================================================================
 #                               variables
 # =======================================================================
 
+horses_list = []
+horses = []
 
 # =======================================================================
 #                                methods
 # =======================================================================
+
+
+def choose_number_of_horses_and_shuffle_them():
+    horses_number = int(input("Indiquez le nombre de chevaux que vous souhaitez voir courir "
+                              ": (entre 12 et 20 chevaux) : "))
+
+    while horses_number < 12 or horses_number > 20:
+        horses_number = int(input("Vous vous êtes trompés, veuillez réessayer : "))
+
+    list_of_horses = list(range(1, horses_number + 1))
+    random.shuffle(list_of_horses)
+    return list_of_horses
 
 
 # ==============================================================
